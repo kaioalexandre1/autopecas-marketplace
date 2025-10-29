a7489aa6263723dfd30837fb3efd35e988a1ca7b
kaioalexandre1 <kaioxander@gmail.com>
2025-10-29 16:08:28 -0300

    new

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -92,7 +92,7 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="bg-blue-700" style={{ backgroundColor: 'rgb(29, 78, 216)', opacity: 1 }}>
+    <nav className="navbar-custom bg-blue-700 relative z-50" style={{ backgroundColor: '#1d4ed8', opacity: 1, position: 'relative', zIndex: 9999 }}>
       <div className="container mx-auto px-4" style={{ opacity: 1 }}>
         <div className="flex items-center justify-between h-20" style={{ opacity: 1 }}>
           {/* Logo */}
