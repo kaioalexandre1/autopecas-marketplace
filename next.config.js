187cdd32625184a9143ad972bdc8cda0d2b0bf47
kaioalexandre1 <kaioxander@gmail.com>
2025-11-03 17:57:50 -0300

    att pag

--- next.config.js
+++ next.config.js
@@ -14,12 +14,13 @@ const nextConfig = {
   async headers() {
     // CSP para desenvolvimento e produção - permitir MercadoPago SDK, Mercado Livre e Google Fonts
     // Configuração permissiva para domínios do Mercado Pago/Mercado Livre para garantir funcionamento completo do pagamento com cartão
-    const cspValue = "default-src 'self' https://*.mercadopago.com https://*.mercadolibre.com https://*.mlstatic.com; " +
-      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.gstatic.com https://www.google.com https://apis.google.com https://firebase.googleapis.com https://sdk.mercadopago.com https://vercel.live https://http2.mlstatic.com https://*.mlstatic.com https://mercadopago.com https://*.mercadopago.com https://mercadolibre.com https://*.mercadolibre.com; " +
-      "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://http2.mlstatic.com https://*.mlstatic.com https://mercadopago.com https://*.mercadopago.com https://mercadolibre.com https://*.mercadolibre.com; " +
-      "font-src 'self' https://fonts.gstatic.com https://http2.mlstatic.com https://*.mlstatic.com https://mercadopago.com https://*.mercadopago.com https://mercadolibre.com https://*.mercadolibre.com data:; " +
-      "img-src 'self' data: https: blob: https://http2.mlstatic.com https://*.mlstatic.com https://mercadopago.com https://*.mercadopago.com https://mercadolibre.com https://*.mercadolibre.com; " +
-      "connect-src 'self' https://*.firebaseio.com https://*.googleapis.com https://api.mercadopago.com https://*.mercadopago.com https://sdk.mercadopago.com https://api.mercadolibre.com https://*.mercadolibre.com https://www.mercadolibre.com https://http2.mlstatic.com https://*.mlstatic.com https://mercadopago.com https://mercadolibre.com; " +
+    // IMPORTANTE: Permitir explicitamente http2.mlstatic.com para fontes do Mercado Pago
+    const cspValue = "default-src 'self' https://*.mercadopago.com https://*.mercadolibre.com https://*.mlstatic.com http://http2.mlstatic.com https://http2.mlstatic.com; " +
+      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.gstatic.com https://www.google.com https://apis.google.com https://firebase.googleapis.com https://sdk.mercadopago.com https://vercel.live https://http2.mlstatic.com https://*.mlstatic.com http://http2.mlstatic.com https://mercadopago.com https://*.mercadopago.com https://mercadolibre.com https://*.mercadolibre.com; " +
+      "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://http2.mlstatic.com https://*.mlstatic.com http://http2.mlstatic.com https://mercadopago.com https://*.mercadopago.com https://mercadolibre.com https://*.mercadolibre.com; " +
+      "font-src 'self' https://fonts.gstatic.com https://http2.mlstatic.com https://*.mlstatic.com http://http2.mlstatic.com http://*.mlstatic.com https://mercadopago.com https://*.mercadopago.com https://mercadolibre.com https://*.mercadolibre.com data: blob:; " +
+      "img-src 'self' data: https: blob: http: https://http2.mlstatic.com https://*.mlstatic.com http://http2.mlstatic.com http://*.mlstatic.com https://mercadopago.com https://*.mercadopago.com https://mercadolibre.com https://*.mercadolibre.com; " +
+      "connect-src 'self' https://*.firebaseio.com https://*.googleapis.com https://api.mercadopago.com https://*.mercadopago.com https://sdk.mercadopago.com https://api.mercadolibre.com https://*.mercadolibre.com https://www.mercadolibre.com https://http2.mlstatic.com https://*.mlstatic.com http://http2.mlstatic.com http://*.mlstatic.com https://mercadopago.com https://mercadolibre.com; " +
       "frame-src 'self' https://www.mercadolibre.com https://*.mercadolibre.com https://vercel.live https://mercadopago.com https://*.mercadopago.com; " +
       "worker-src 'self' blob:;";
     
