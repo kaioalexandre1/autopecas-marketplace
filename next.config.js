1d73956ebe1a77b24d74a86fe52b9bac91b2366a
kaioalexandre1 <kaioxander@gmail.com>
2025-11-03 17:51:31 -0300

    att pagamentos

--- next.config.js
+++ next.config.js
@@ -13,14 +13,15 @@ const nextConfig = {
   // Headers de segurança - permitir recursos necessários para MercadoPago SDK e Google Fonts
   async headers() {
     // CSP para desenvolvimento e produção - permitir MercadoPago SDK, Mercado Livre e Google Fonts
-    // Configuração mais permissiva para domínios do Mercado Pago/Mercado Livre para garantir funcionamento completo
-    const cspValue = "default-src 'self'; " +
-      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.gstatic.com https://www.google.com https://apis.google.com https://firebase.googleapis.com https://sdk.mercadopago.com https://vercel.live https://http2.mlstatic.com https://*.mlstatic.com; " +
-      "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://http2.mlstatic.com https://*.mlstatic.com https://mercadopago.com https://*.mercadopago.com; " +
-      "font-src 'self' https://fonts.gstatic.com https://http2.mlstatic.com https://*.mlstatic.com https://mercadopago.com https://*.mercadopago.com data:; " +
-      "img-src 'self' data: https: blob: https://http2.mlstatic.com https://*.mlstatic.com https://mercadopago.com https://*.mercadopago.com; " +
-      "connect-src 'self' https://*.firebaseio.com https://*.googleapis.com https://api.mercadopago.com https://*.mercadopago.com https://sdk.mercadopago.com https://api.mercadolibre.com https://*.mercadolibre.com https://www.mercadolibre.com https://http2.mlstatic.com https://*.mlstatic.com https://mercadopago.com; " +
-      "frame-src 'self' https://www.mercadolibre.com https://*.mercadolibre.com https://vercel.live https://mercadopago.com https://*.mercadopago.com;";
+    // Configuração permissiva para domínios do Mercado Pago/Mercado Livre para garantir funcionamento completo do pagamento com cartão
+    const cspValue = "default-src 'self' https://*.mercadopago.com https://*.mercadolibre.com https://*.mlstatic.com; " +
+      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.gstatic.com https://www.google.com https://apis.google.com https://firebase.googleapis.com https://sdk.mercadopago.com https://vercel.live https://http2.mlstatic.com https://*.mlstatic.com https://mercadopago.com https://*.mercadopago.com https://mercadolibre.com https://*.mercadolibre.com; " +
+      "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://http2.mlstatic.com https://*.mlstatic.com https://mercadopago.com https://*.mercadopago.com https://mercadolibre.com https://*.mercadolibre.com; " +
+      "font-src 'self' https://fonts.gstatic.com https://http2.mlstatic.com https://*.mlstatic.com https://mercadopago.com https://*.mercadopago.com https://mercadolibre.com https://*.mercadolibre.com data:; " +
+      "img-src 'self' data: https: blob: https://http2.mlstatic.com https://*.mlstatic.com https://mercadopago.com https://*.mercadopago.com https://mercadolibre.com https://*.mercadolibre.com; " +
+      "connect-src 'self' https://*.firebaseio.com https://*.googleapis.com https://api.mercadopago.com https://*.mercadopago.com https://sdk.mercadopago.com https://api.mercadolibre.com https://*.mercadolibre.com https://www.mercadolibre.com https://http2.mlstatic.com https://*.mlstatic.com https://mercadopago.com https://mercadolibre.com; " +
+      "frame-src 'self' https://www.mercadolibre.com https://*.mercadolibre.com https://vercel.live https://mercadopago.com https://*.mercadopago.com; " +
+      "worker-src 'self' blob:;";
     
     return [
       {
